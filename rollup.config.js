8d89b6d31fa69b17854b0b01250791f5ae3edbf2
mitchellhamilton <mitchell@mitchellhamilton.me>
2018-01-29 16:49:56 +1000

    Use babel instead of rollup because the rollup build is broken

--- rollup.config.js
+++ rollup.config.js
@@ -10,23 +10,29 @@ const pkg = require(path.resolve(process.cwd(), './package.json'))
 
 const config = {
   input: './src/index.js',
-  external: ['react', 'new-css-in-js'],
+  external: [
+    'react',
+    'new-css-in-js',
+    'emotion-utils',
+    'stylis-rule-sheet',
+    'prop-types',
+    'create-react-context'
+  ],
   exports: 'named',
   sourcemap: true,
   plugins: [
     babel({
       presets: [
         [
-          'env',
+          '@babel/env',
           {
             loose: true,
-            modules: false,
-            exclude: ['transform-es2015-typeof-symbol']
+            modules: false
           }
         ],
-        'flow',
-        'react',
-        'stage-0'
+        '@babel/flow',
+        '@babel/react',
+        '@babel/stage-0'
       ],
       babelrc: false
     }),
@@ -38,11 +44,10 @@ const config = {
     { file: pkg.module, format: 'es' }
   ]
 }
-
-if (process.env.UMD) {
-  config.external = ['react']
-  config.globals = { react: 'React' }
-  config.plugins.push(
+const umdConfig = Object.assign({}, config, {
+  external: ['react'],
+  globals: { react: 'React' },
+  plugins: config.plugins.concat(
     alias({
       'new-css-in-js': path.resolve(__dirname, './packages/core/src/index.js')
     }),
@@ -50,20 +55,12 @@ if (process.env.UMD) {
       'process.env.NODE_ENV': JSON.stringify('production')
     }),
     uglify()
-  )
-  config.output = [
-    {
-      file: './dist/DO-NOT-USE.min.js',
-      format: 'umd',
-      name: pkg.name
-    }
-  ]
-}
+  ),
+  output: {
+    file: './dist/DO-NOT-USE.min.js',
+    format: 'umd',
+    name: pkg.name
+  }
+})
 
-if (pkg.name === 'preact-new-css-in-js') {
-  config.input = '../react/src/index.js'
-  config.external = ['preact', 'new-css-in-js']
-  config.plugins.unshift(alias({ react: 'preact' }))
-}
-
-export default config
+export default [config, umdConfig]
