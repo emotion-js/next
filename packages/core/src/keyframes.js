b09251e2dd8c3c43ae37e65ccefe7ce6eed8c359
mitchellhamilton <mitchell@mitchellhamilton.me>
2018-02-08 21:11:37 +1000

    Change some types

--- packages/core/src/keyframes.js
+++ packages/core/src/keyframes.js
@@ -1,11 +1,17 @@
 // @flow
 import * as React from 'react'
 import { CSSContext } from './context'
-import type { CSSContextType, InsertableStyles } from './types'
+import type {
+  CSSContextType,
+  ScopedInsertableStyles,
+  KeyframesInsertableStyles
+} from './types'
 import { hydration, isBrowser, insertStyles } from './utils'
 import { serializeStyles } from './serialize'
 
-export const keyframes = (arg: InsertableStyles): InsertableStyles => {
+export const keyframes = (
+  arg: ScopedInsertableStyles
+): KeyframesInsertableStyles => {
   const name = `animation-${arg.name}`
   const ret = {
     styles: `@keyframes ${name}{${arg.styles}}`,
