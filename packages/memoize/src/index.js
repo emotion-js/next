7f61f104af1e1451cf0c03406b0ed397c2e1c017
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-04-07 23:31:30 +0200

    Added @emotion/memoize package (#3)

---
+++ packages/memoize/src/index.js
@@ -0,0 +1,9 @@
+// @flow
+
+export default function memoize(fn: string => any) {
+  const cache = {}
+  return (arg: string) => {
+    if (cache[arg] === undefined) cache[arg] = fn(arg)
+    return cache[arg]
+  }
+}
