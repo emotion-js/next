bfd87494d7cf78a4d12ce9e4e9b24400db80a54e
mitchellhamilton <mitchell@mitchellhamilton.me>
2018-04-08 08:42:41 +1000

    Commit to get lerna to publish

--- packages/memoize/src/index.js
+++ packages/memoize/src/index.js
@@ -2,6 +2,7 @@
 
 export default function memoize(fn: string => any) {
   const cache = {}
+
   return (arg: string) => {
     if (cache[arg] === undefined) cache[arg] = fn(arg)
     return cache[arg]
