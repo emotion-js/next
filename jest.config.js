5d32647cc9c2c973ae456f6c2efac8f393ea7457
mitchellhamilton <mitchell@mitchellhamilton.me>
2018-06-23 11:07:12 +1000

    Update jest

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,10 @@ module.exports = {
     '<rootDir>/scripts/*/__tests__/**/*.{js,jsx,mjs}',
     '<rootDir>/scripts/**/*/__tests__/**/*.{js,jsx,mjs}'
   ],
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname'
+  ],
   testPathIgnorePatterns: ['/node_modules/', '/__fixtures__/'],
   moduleNameMapper: require('lerna-alias').jest(),
   setupTestFrameworkScriptFile: '<rootDir>/test/setup.js',
