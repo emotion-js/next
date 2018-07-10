74bee168275e8adda2a92c468b1da12e31fc3e97
mitchellhamilton <mitchell@mitchellhamilton.me>
2018-07-11 09:14:18 +1000

    Change tag list

--- packages/styled/src/index.js
+++ packages/styled/src/index.js
@@ -1,18 +1,9 @@
 // @flow
 import styled from '@emotion/styled-base'
-
-declare var codegen: Function
-
-const tags = codegen`
-const htmlTagNames = require('html-tag-names')
-const svgTagNames = require('svg-tag-names')
-module.exports = JSON.stringify(htmlTagNames
-.concat(svgTagNames)
-.filter((tag, index, array) => array.indexOf(tag) === index))
-`
+import { tags } from './tags'
 
 // bind it to avoid mutating the original function
-const newStyled = styled.bind(undefined)
+const newStyled = styled.bind()
 
 tags.forEach(tagName => {
   newStyled[tagName] = newStyled(tagName)
