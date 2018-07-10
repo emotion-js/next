a43137d30271362dd334429be6492e50a2dc5ab8
mitchellhamilton <mitchell@mitchellhamilton.me>
2018-07-10 16:59:34 +1000

    Update cra example

--- examples/cra-example/src/index.js
+++ examples/cra-example/src/index.js
@@ -1,13 +1,39 @@
 // @flow
+/** @jsx jsx */
+import Provider from '@emotion/provider'
+import { jsx } from '@emotion/core'
 import React from 'react'
 import { Global } from '@emotion/core'
 import ReactDOM from 'react-dom'
 import App from './App'
 // import registerServiceWorker from './registerServiceWorker'
 
+class ThemeTest extends React.Component<*, *> {
+  state = { theme: { color: 'hotpink', margin: 4 } }
+  render() {
+    return (
+      <Provider theme={this.state.theme}>
+        <button
+          id="the-thing"
+          onClick={() => {
+            this.setState({ theme: { color: 'green', margin: 8 } })
+          }}
+          css={({ color, margin }) => ({
+            color,
+            margin
+          })}
+        >
+          something
+        </button>
+      </Provider>
+    )
+  }
+}
+
 ReactDOM.render(
   <React.Fragment>
     <App />
+    <ThemeTest />
     <Global
       styles={{
         body: {
